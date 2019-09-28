792d59f75661b27babca8a140d2b5a5e9e0cd14b
Will Ware <wware@alum.mit.edu>
2019-09-28 11:30:54 -0400

    x

--- hack.py
+++ hack.py
@@ -58,10 +58,24 @@ def FIXMULT(fix1, fix2):
 
 _table_size = 4096
 _sine_table = [
-    math.sin(2 * math.pi * i / _table_size)
+    FIXED(math.sin(2 * math.pi * i / _table_size))
     for i in xrange(_table_size)
 ]
 
 class Sinusoid(object):
     def __init__(self):
         self._ampl = self._phase = FIXED(0.)
+        self._da = self._dph = FIXED(0.)
+
+    def set(self, da, dph):
+        self._da = da
+	self._dph = dph
+
+    def iterate(self, buf):
+        for i in range(RETENTION_RATIO):
+	    x = buf[i]
+	    a, ph = self._a, self._ph
+	    new_a, new_ph = a + self._da, ph + self._dph
+            table_index = int((a & FRACBITS) * _table_size) / >> FRACBITS
+	    assert 0 <= table_index < _table_size
+	    sval = _sine_table
